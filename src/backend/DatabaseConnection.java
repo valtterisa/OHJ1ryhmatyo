0a7779ff2b959b5d7cfd3c6ebc08ae945490867b
akulau <akulau@student.uef.fi>
2022-04-20 22:57:21 +0300

    Lisätty toiminnallisuus poistolle ja päivittämiselle. Kommentoitu koodia ja päivitetty backendin readme

--- src/backend/DatabaseConnection.java
+++ src/backend/DatabaseConnection.java
@@ -56,6 +56,18 @@ public class DatabaseConnection {
         }
     }
 
+    /**
+     * Muodostaa yhteyden tietokantaan ja tekee sinne dynaamisesti generoidun SQL-kyselyn
+     * annetuista parametreista. Lopuksi sulkee tietokantayhteyden, sekä palauttaa tietokannasta
+     * saadun datan, tai tietoa tuloksesta kaksiuloitteidessa ArrayList:ssä.
+     * <p>Metodit REST-rajapinnan mukaan: GET (select),
+     * POST (insert), <s>PUT (update)</s>, DELETE (delete)</p>
+     *
+     * @param method  String-muuttuja; joko: <b>get, post tai delete</b>
+     * @param table String-muuttuja; Tietokannan taulun tai näkymän nimi.
+     * @param parameters HashMap<String, String>; Parametrit avain-arvo -pareina.
+     * @return Data kaksiuloitteisessa ArrayList:ssä
+     */
     public ArrayList doSQL(String method, String table, HashMap<String, String> parameters) {
 
         ArrayList<ArrayList<String>> list = new ArrayList<>();
@@ -64,57 +76,133 @@ public class DatabaseConnection {
         System.out.println("Making query: " + sql);
 
         try {
-            PreparedStatement statement = this.connect().prepareStatement(sql); // Asettaa generoidun SQL-lauseen statement-oliolle
+            PreparedStatement statement = this.connect().prepareStatement(sql,  // Asettaa generoidun SQL-lauseen statement-oliolle
+                                                Statement.RETURN_GENERATED_KEYS);
+
+            int i = 0;
+            for (String str : parameters.keySet()) {                             // Parametroi SQL-lauseen ?-muuttujat
+                statement.setString(i+1, parameters.get(str));
+                i++;
+            }
+
+
+            if (method.equalsIgnoreCase("get")) {                     // METHOD = GET
+                ResultSet res = statement.executeQuery();                        // res-muuttujassa queryn palauttama data
+
+                int row = 0;
+                while(res.next()) {                                              // käy läpi res-muuttujan ja mäppää taulukon kaksiuloitteeseen ArrayListiin
+                    list.add(new ArrayList());
+                    for (int col = 0; col < (res.getMetaData().getColumnCount()); col++) {
+                        list.get(row).add(res.getString(res.getMetaData().getColumnLabel(col + 1)));
+                    }
+                    row++;
+                }
+            }
+
+            if (method.equalsIgnoreCase("post")) {                   // METHOD = POST
+                int res = statement.executeUpdate();                            // res-muuttujassa päivitetty rivi
+
+                ResultSet insertIdRes = statement.getGeneratedKeys();
+
+                String insertIdColumn = null;
+                int insertId = -1;
+                while (insertIdRes.next()) {
+                    insertId = insertIdRes.getInt(1);
+                }
+
+                System.out.println("Insert successful, affected rows: " + res + ", inserted id: " + insertId);
+                list.clear();
+                ArrayList<String> success = new ArrayList<>();
+                success.add("Insert successful");                               // palautetaan listassa viesti, lisättyjen rivien määrä ja lisätyn rivin id
+                success.add("" + res);
+                success.add("" + insertId);
+                list.add(success);
+            }
+
+            if (method.equalsIgnoreCase("delete")) {                 // METHOD = DELETE
+                int res = statement.executeUpdate();                            // res-muuttujassa poistettujen rivien määrä
+                System.out.println("Delete successful, affected rows: " + res);
+                list.clear();
+                ArrayList<String> success = new ArrayList<>();
+                success.add("Delete successful");                               // palautetaan listassa viesti ja poistetut rivit
+                success.add("" + res);
+                list.add(success);
+            }
+
+        } catch (SQLException e) {                                              // Virheenkäsittelyä
+            e.printStackTrace();
+        } finally {                                                             // Lopuksi katkaisee tietokantayhteyden
+            this.disconnect();
+        }
+
+        return list;                                                            // Palauttaa listan
+    }
+
+    /**
+     * Muodostaa yhteyden tietokantaan ja tekee sinne dynaamisesti generoidun SQL-kyselyn
+     * annetuista parametreista. Lopuksi sulkee tietokantayhteyden, sekä palauttaa tietokannasta
+     * saadun datan, tai tietoa tuloksesta kaksiuloitteidessa ArrayList:ssä.
+     * <p>Metodit REST-rajapinnan mukaan: GET (select),
+     * POST (insert), PUT (update), DELETE (delete)</p>
+     *
+     * @param method  String-muuttuja; joko: <b>get, post, put tai delete</b>
+     * @param table String-muuttuja; Tietokannan taulun tai näkymän nimi.
+     * @param parameters HashMap<String, String>; Parametrit avain-arvo -pareina.
+     * @param id String[]; Manipuloitavan sarakkeen id:n avain ja arvo taulukossa.
+     * @return Data kaksiuloitteisessa ArrayList:ssä
+     */
+    public ArrayList doSQL(String method, String table, HashMap<String, String> parameters, String[] id) {
+
+        ArrayList<ArrayList<String>> list = new ArrayList<>();
+
+        if (!method.equalsIgnoreCase("put")) {                       // Jos metodi ei ole put, niin ignoraa id-parametrin
+            list = doSQL(method, table, parameters);
+            return list;
+        }
+                                                                                //METHOD = PUT
+        String sql = SQLGenerator.generateSQL(method, table, parameters, id);   // Tekee parametroitavan SQL-lauseen annetuista tiedoista
+        System.out.println("Making query: " + sql);
+
+        try {
+            PreparedStatement statement = this.connect().prepareStatement(sql, // Asettaa generoidun SQL-lauseen statement-oliolle
+                                                Statement.RETURN_GENERATED_KEYS);
 
             int i = 0;
             for (String str : parameters.keySet()) {                            // Parametroi SQL-lauseen ?-muuttujat
                 statement.setString(i+1, parameters.get(str));
                 i++;
             }
+            statement.setString(i+1, id[1]);
 
+            int res = statement.executeUpdate();                                // res-muuttujassa updaten palauttama data
 
-            if (method.equalsIgnoreCase("get")) {            // METHOD = GET
-                try {
-                    ResultSet res = statement.executeQuery();           // res-muuttujassa queryn palauttama data
+            System.out.println("Update successful, affected rows: " + res);
+            HashMap<String, String> putReturn = new HashMap<>();
+            putReturn.put(id[0], id[1]);
 
-                    int row = 0;
-                    while(res.next()) {                                 // käy läpi res-muuttujan ja mäppää taulukon kaksiuloitteeseen ArrayListiin
-                        list.add(new ArrayList());
-                        for (int col = 0; col < (res.getMetaData().getColumnCount()); col++) {
-                            list.get(row).add(res.getString(res.getMetaData().getColumnLabel(col + 1)));
-                        }
-                        row++;
-                    }
-                } catch (SQLException e) {                              // Virheenkäsittelyä
-                    e.printStackTrace();
-                }
-            }
+            list = this.doSQL("get", table, putReturn);                  // palautetaan listassa muokattu rivi tietokannasta
 
-            if (method.equalsIgnoreCase("post")) {           // METHOD = POST
-                int res = statement.executeUpdate();                    // res-muuttujassa updaten palauttama data
-                System.out.println("Insert successful, affected rows: " + res);
-            }
-                } catch (SQLException e) {                              // Virheenkäsittelyä
+        } catch (SQLException e) {                                              // Virheenkäsittelyä
             e.printStackTrace();
-        } finally {                                                     // Lopuksi katkaisee tietokantayhteyden
+        } finally {                                                             // Lopuksi katkaisee tietokantayhteyden
             this.disconnect();
         }
 
-        return list;                                                    // Palauttaa listan
+        return list;                                                            // Palauttaa listan
     }
 
 
     /*Tietokantayhteyden testailua*/
     public static void main(String[] args) {
 
-        DatabaseConnection db = new DatabaseConnection();               // Tietokantayhteys
+        DatabaseConnection db = new DatabaseConnection();                       // Tietokantayhteys
 
-        HashMap<String, String> hakuehdot = new HashMap();              // Haku rajatuilla ehdoilla
+        HashMap<String, String> hakuehdot = new HashMap();                      // Haku rajatuilla ehdoilla
         hakuehdot.put("etunimi", "Kullervo");
         hakuehdot.put("postinro", "18890");
 
-        HashMap<String, String> uusiAsiakas = new HashMap();            // Tiedot uudelle lisättävälle asiakkaalle
-        //asiakasParam.put("asiakas_id", "10");                         // Tietokannassa auto-increment, joten ei tarvitse
+        HashMap<String, String> uusiAsiakas = new HashMap();                    // Tiedot uudelle lisättävälle asiakkaalle
+        //asiakasParam.put("asiakas_id", "10");                                 // Tietokannassa auto-increment, joten ei tarvitse
         uusiAsiakas.put("postinro", "18890");
         uusiAsiakas.put("etunimi", "Kullervo");
         uusiAsiakas.put("sukunimi", "Kulkija");
@@ -122,10 +210,25 @@ public class DatabaseConnection {
         uusiAsiakas.put("email", "kullervo.kulkija@outlook.com");
         uusiAsiakas.put("puhelinnro", "7777777777");
 
-        System.out.println("GET (kaikki): " + db.doSQL("get", "mokki", new HashMap<>()));
+        HashMap<String, String> muokattavaAsiakas = new HashMap();              // Tiedot muokatavalle asiakkaalle
+        //muokattavaAsiakas.put("asiakas_id", "2");                             // muokattavan asiakkaan ID tulee omassa kentässään. EI TÄHÄN
+        muokattavaAsiakas.put("postinro", "70150");
+        muokattavaAsiakas.put("etunimi", "Pertti");
+        muokattavaAsiakas.put("sukunimi", "Perämetsä");
+        muokattavaAsiakas.put("lahiosoite", "Perämetsäpolku 3");
+        muokattavaAsiakas.put("email", "Pertti.Perämetsä@gmail.com");
+        muokattavaAsiakas.put("puhelinnro", "1231231230");
+
+        String[] muokattavaID = new String[] {"asiakas_id", "2"};               // Muokattavan asiakkaan ID
+
+        HashMap<String, String> poistettavaID = new HashMap<>();                // Poistettavan / Poistettavien asiakkaiden tiedot
+        poistettavaID.put("asiakas_id", "37");                                  // esim. id
+
+        System.out.println("GET (kaikki): " + db.doSQL("get", "mokki", new HashMap<>(), muokattavaID));
         System.out.println("GET (rajattu): " + db.doSQL("get", "asiakas", hakuehdot));
         System.out.println("POST (uusi asiakas): " + db.doSQL("post", "asiakas", uusiAsiakas));
-
+        System.out.println("PUT (update): " + db.doSQL("put", "asiakas", muokattavaAsiakas, muokattavaID));
+        System.out.println("DELETE (delete): " + db.doSQL("delete", "asiakas", poistettavaID));
     }
 }
 
--- src/backend/SQLGenerator.java
+++ src/backend/SQLGenerator.java
@@ -7,60 +7,115 @@ public class SQLGenerator {
     private static String generateGet(String table, HashMap<String, String> parameters) {
         StringBuilder sql;
         sql = new StringBuilder("SELECT * ");
-        sql.append("FROM ").append(table).append(" WHERE (1=1)");        // Koska WHERE (1=1), niin helppo lisätä AND
-        for (String param : parameters.keySet()) {                       // jos hakuehtoja, niin parametroitavat ? -muuttujat
+        sql.append("FROM ").append(table).append(" WHERE (1=1)");           // Koska WHERE (1=1), niin helppo lisätä AND
+        for (String param : parameters.keySet()) {                          // jos hakuehtoja, niin parametroitavat ? -muuttujat
             sql.append(" AND (").append(param).append(" = ?)");
         }
-        return sql.toString();                                          // Palauttaa SQL-lauseen String-muuttujassa
+        return sql.toString();                                              // Palauttaa SQL-lauseen String-muuttujassa
     }
 
     private static String generatePost(String table, HashMap<String, String> parameters) {
         StringBuilder sql;
         sql = new StringBuilder("INSERT INTO " + table + " (");
-        for (String str : parameters.keySet()) {                        // sarakkeiden nimet
+        for (String str : parameters.keySet()) {                            // sarakkeiden nimet
             sql.append(str).append(",");
         }
-        if (sql.charAt(sql.length() - 1) == ',') {                      // Jos lopussa pilkku, poistaa sen
+        if (sql.charAt(sql.length() - 1) == ',') {                          // Jos lopussa pilkku, poistaa sen
             sql = new StringBuilder(sql.substring(0, sql.length() - 1));
         }
         sql.append(") VALUES (");
-        for (int i = 0; i < parameters.size(); i++) {                   // parametroitavat ? -muuttujat
+        for (int i = 0; i < parameters.size(); i++) {                       // parametroitavat ? -muuttujat
             sql.append("?,");
         }
-        if (sql.charAt(sql.length() - 1) == ',') {                      // Jos lopussa pilkku, poistaa sen
+        if (sql.charAt(sql.length() - 1) == ',') {                          // Jos lopussa pilkku, poistaa sen
             sql = new StringBuilder(sql.substring(0, sql.length() - 1));
         }
         sql.append(")");
-        return sql.toString();                                          // Palauttaa SQL-lauseen String-muuttujassa
+        return sql.toString();                                              // Palauttaa SQL-lauseen String-muuttujassa
+    }
+
+    private static String generatePut(String table, HashMap<String, String> parameters, String[] id) {
+        StringBuilder sql;
+        sql = new StringBuilder("UPDATE " + table + " SET ");
+        for (String str : parameters.keySet()) {                            // sarakkeiden nimet
+            sql.append(str).append(" = ?,");
+        }
+        if (sql.charAt(sql.length() - 1) == ',') {                          // Jos lopussa pilkku, poistaa sen
+            sql = new StringBuilder(sql.substring(0, sql.length() - 1));
+        }
+        sql.append(" WHERE (" + id[0] + " = ?)");
+        return sql.toString();                                              // Palauttaa SQL-lauseen String-muuttujassa
+    }
+
+    private static String generateDelete(String table, HashMap<String, String> parameters) {
+        StringBuilder sql;
+        sql = new StringBuilder("DELETE FROM " + table);
+        sql.append(" WHERE (1=1)");                                         // Koska WHERE (1=1), niin helppo lisätä AND
+        for (String param : parameters.keySet()) {                          // jos hakuehtoja, niin parametroitavat ? -muuttujat
+            sql.append(" AND (").append(param).append(" = ?)");
+        }
+        return sql.toString();                                              // Palauttaa SQL-lauseen String-muuttujassa
     }
 
     /**
-     * Generoi SQL-lauseen anetuisat parametreista.
+     * Generoi SQL-lauseen anetuista parametreista.
      * Palauttaa generoidun SQL-lauseen String-oliossa.
+     * <p>Metodit REST-rajapinnan mukaan: GET (select),
+     * POST (insert), <s>PUT (update)</s>, DELETE (delete)</p>
      *
-     * @param method  String-muuttuja; joko: get, post
-     * @param table String-muuttuja; Tietokannan taulun tai näkymän nimi
-     * @param parameters HashMap<String, String>; Parametrit avain-arvo -pareina
-     * @return      SQL-lause String-muuttujassa
+     * @param method  String-muuttuja; joko: <b> get, post tai delete </b>
+     * @param table String-muuttuja; Tietokannan taulun tai näkymän nimi.
+     * @param parameters HashMap<String, String>; Parametrit avain-arvo -pareina.
+     * @return SQL-lause String-muuttujassa
      */
     public static String generateSQL(String method, String table, HashMap<String, String> parameters) {
         System.out.println("Generating SQL...");
         String sql = null;
 
-        if (method.equalsIgnoreCase("get")) {                // METHOD = GET
+        if (method.equalsIgnoreCase("get")) {                   // METHOD = GET
             sql = generateGet(table, parameters);
         }
 
-        else if (method.equalsIgnoreCase("post")) {          // METHOD = POST
+        else if (method.equalsIgnoreCase("post")) {             // METHOD = POST
             sql = generatePost(table, parameters);
         }
 
-        else {                                                          // Jos metodi on tuntematon, printtaa erroria ja palauttaa nullia
+        else if (method.equalsIgnoreCase("delete")) {             // METHOD = DELETE
+            sql = generateDelete(table, parameters);
+        }
+        else {                                                              // Jos metodi on tuntematon, printtaa erroria ja palauttaa nullia
             System.out.println("Error generating SQL. Unknown method: \"" + method + "\"");
             return null;
         }
 
-        System.out.println("Generated SQL: " + sql);                    // Jos kaikki ok, palauttaa generoidun SQL-lauseen
+        System.out.println("Generated SQL: " + sql);                        // Jos kaikki ok, palauttaa generoidun SQL-lauseen
+        return sql;
+    }
+
+    /**
+     * Generoi SQL-lauseen anetuista parametreista.
+     * Palauttaa generoidun SQL-lauseen String-oliossa.
+     * <p>Metodit REST-rajapinnan mukaan: GET (select),
+     * POST (insert), PUT (update), DELETE (delete)</p>
+     *
+     * @param method  String-muuttuja; joko: <b> get, post, put tai delete </b>
+     * @param table String-muuttuja; Tietokannan taulun tai näkymän nimi.
+     * @param parameters HashMap<String, String>; Parametrit avain-arvo -pareina.
+     * @param id String[]; Manipuloitavan sarakkeen id:n avain ja arvo taulukossa.
+     * @return SQL-lause String-muuttujassa
+     */
+    public static String generateSQL(String method, String table, HashMap<String, String> parameters, String[] id) {
+        System.out.println("Generating SQL...");
+        String sql = null;
+
+        if (method.equalsIgnoreCase("put")) {                   // METHOD = PUT
+            sql = generatePut(table, parameters, id);
+        }
+        else {                                                              // Jos metodi on muu kuin put, ei ota huomioon id-kenttää
+            sql = generateSQL(method, table, parameters);
+        }
+
+        System.out.println("Generated SQL: " + sql);                        // Jos kaikki ok, palauttaa generoidun SQL-lauseen
         return sql;
     }
 }
